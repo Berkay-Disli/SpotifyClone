17ee6b6a69e9db083a7ba9010982156252954743
Berkay-Disli <berkaydisli.work@gmail.com>
2022-09-06 14:14:54 +0300

    Initial Commit

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Berkay Disli on 6.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
